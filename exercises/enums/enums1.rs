ae53d822b26a964633ec9f206603205611ef4a03
MIUSUN001 <196221625@qq.com>
2025-04-02 17:27:39 +0800

    solve neums1

--- exercises/enums/enums1.rs
+++ exercises/enums/enums1.rs
@@ -7,11 +7,15 @@
 #[derive(Debug)]
 enum Message {
     // TODO: define a few types of messages as used below
+    Quit,
+    Echo(String),
+    Move(i32,i32),
+    ChangeColor(u8,u8,u8)
 }
 
 fn main() {
     println!("{:?}", Message::Quit);
-    println!("{:?}", Message::Echo);
-    println!("{:?}", Message::Move);
-    println!("{:?}", Message::ChangeColor);
+    println!("{:?}", Message::Echo("hello world".to_string()));
+    println!("{:?}", Message::Move(10,20));
+    println!("{:?}", Message::ChangeColor(255,0,0));
 }
