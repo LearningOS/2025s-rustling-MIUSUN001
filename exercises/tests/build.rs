d352159ba700764303be38fc235e7e6e7f9bf2f0
MIUSUN001 <196221625@qq.com>
2025-04-11 16:17:19 +0800

    tests7

--- exercises/tests/build.rs
+++ exercises/tests/build.rs
@@ -5,6 +5,17 @@
 use std::env;
 
 fn main() {
+    use std::time::SystemTime;
+    use std::time::UNIX_EPOCH;
+
+    // 获取当前时间戳
+    let start = SystemTime::now();
+    let since_the_epoch = start.duration_since(UNIX_EPOCH)
+        .expect("Time went backwards")
+        .as_secs();
+
+    // 设置环境变量TEST_FOO
+    println!("cargo:rustc-env=TEST_FOO={}", since_the_epoch);
     // 获取当前包的名称
     let pkg_name = env::var("CARGO_PKG_NAME").unwrap();
 
